2ad6cbfa467a9f8b5a0e3c0ca708e9835e3c5a49
Túlio Di Sabatino <tuliosabatino@gmail.com>
2020-06-10 18:52:50 +0100

    Backend: Added 'IF EXISTIS' to table drop command Added docstring documentation

--- event_handler.py
+++ event_handler.py
@@ -1,10 +1,18 @@
 import sqlite3
 from datetime import datetime
 
-class EventHandler(object):
+class EventHandler():
+    """
+    Class responsible for interacting with SQLite database containing events occuring within the company
+    """
     
     def __init__(self, dbFile:str = 'events.db'):
+        """
+        Constructor
 
+        Args:
+            dbFile (str, optional): Absolute or relative path to the database. Defaults to 'events.db'.
+        """
         self.dbPath = dbFile
         
         self.tableName = 'events'
@@ -14,11 +22,17 @@ class EventHandler(object):
         self.eventDescription = 'eventDescription'
 
     def createDatabase(self, dropTable:bool = False):
+        """
+        Creates the events table in the database if it does not exist. 
+        Can optionally clear previously existing table.
 
+        Args:
+            dropTable (bool, optional): Set to true to clear previously existing table. Defaults to False.
+        """
         conn = sqlite3.connect(self.dbPath)
 
         if dropTable:
-            conn.execute(f'''DROP TABLE {self.tableName}''')
+            conn.execute(f'''DROP TABLE IF EXISTS {self.tableName}''')
 
         conn.execute(f'''
                         CREATE TABLE IF NOT EXISTS {self.tableName}
@@ -27,7 +41,14 @@ class EventHandler(object):
         conn.commit()
 
     def insertEvent(self, eventType:str, eventTitle:str, eventDescription:str):
-        
+        """
+        Inserts an event into the events table.
+
+        Args:
+            eventType (str): Type of the event
+            eventTitle (str): Title of the event
+            eventDescription (str): Description of the event
+        """ 
         conn = sqlite3.connect(self.dbPath)
 
         eventDate = datetime.utcnow().strftime("%m/%d/%Y, %H:%M:%S")
@@ -38,26 +59,52 @@ class EventHandler(object):
         ''')
         conn.commit()
 
-    def _dict_factory(self, cursor, row):
+    def _dict_factory(self, cursor, row) -> dict:
+        """
+        Converts SQL query result into a dictionary to facilitate processing into JSON
+
+        Args:
+            cursor : Cursor object
+            row : Tuple containing the original row of the query
+
+        Returns:
+            dict: Dictionary containing the data of the provided row
+        """
         d = {}
         for idx, col in enumerate(cursor.description):
             d[col[0]] = row[idx]
         return d
 
-    def getAllEvents(self) -> dict:
+    def getAllEvents(self) -> list:
+        """
+        Retrieves all the events stored in the table
 
+        Returns:
+            list: List of containing the events stored in the table. Each event is a dictionary
+        """
         conn = sqlite3.connect(self.dbPath)
         conn.row_factory = self._dict_factory
         cur = conn.cursor()
 
         return cur.execute(f'SELECT * FROM {self.tableName} ORDER BY {self.date} DESC;').fetchall()
 
-    def getEventByFilter(self, query_parameters: dict) -> dict:
-        
+    def getEventByFilter(self, query_parameters: dict) -> list:
+        """
+        Retrieves all the events matching the specified filters 
+        for the 'date', 'eventType' and 'eventTitle' parameters.
+
+        Args:
+            query_parameters (dict): Dictionary containing the filter specifications. 
+                                     Only the keys 'date', 'eventType' and 'eventTitle' will be processed.
+
+        Returns:
+            list: List of containing the events stored in the table. Each event is a dictionary
+        """
         date = query_parameters.get(self.date)
         eventType = query_parameters.get(self.eventType)
         eventTitle = query_parameters.get(self.eventTitle)
 
+        # Assemble SQL query based on the specified parameters
         query = f"SELECT * FROM {self.tableName} WHERE"
         to_filter = []
 
